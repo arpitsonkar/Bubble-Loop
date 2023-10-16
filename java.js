7fee55ba0d268e5092ae02cd17bc3942b1dc6c70
arpitsonkar <146727361+arpitsonkar@users.noreply.github.com>
2023-10-16 19:40:04 +0530

    Bubble Loop
    Today date is 16 October and I started 100 Day's Challenge for me.
    So it's first day and I make this awesome Project with Html,Css and JavaScript.
    If you like this game please give star for my Projects.

---
+++ java.js
@@ -0,0 +1,49 @@
+var hitrn = 0;
+var timer = 60;
+var score = 0; // Set the initial score to 0
+
+function makeBubbble(){
+    var clutter = "";
+
+    for(var i = 1; i <= 55; i++){
+        var rn = Math.floor(Math.random() * 10);
+        clutter += `<div class="bubble">${rn}</div>`;
+    }
+    document.querySelector("#btmcontainer").innerHTML = clutter;
+}
+
+function runTime(){
+    var timeint = setInterval(function(){
+        if(timer > 0){
+            timer--;
+            document.querySelector("#timeval").textContent = timer;
+        }else{
+            clearInterval(timeint); // Corrected the typo here, changed "|" to "||"
+            document.querySelector("#btmcontainer").innerHTML = `<h1>Game Over Press F5</h1>`;
+        }
+    }, 1000);
+}
+
+function getNewHit(){
+    hitrn = Math.floor(Math.random() * 10);
+    document.querySelector("#hitval").textContent = hitrn;
+}
+
+function upscore(){
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
+}
+
+document.querySelector("#btmcontainer").addEventListener("click", function(arpit){
+    var click = Number(arpit.target.textContent);
+    if(click === hitrn){
+        upscore();
+        makeBubbble();
+        getNewHit();
+    }
+})
+
+getNewHit();
+makeBubbble();
+runTime();
+score = 0; // Set the score to 0 initially
